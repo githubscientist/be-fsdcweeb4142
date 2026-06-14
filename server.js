a78301777717ca0f93fab9ded96b9c9e80d08c68
githubscientist <sathish.datascientist@gmail.com>
2026-06-14 12:43:09 +0530

    restful api setup

--- server.js
+++ server.js
@@ -3,26 +3,46 @@ const http = require('http');
 
 // create a http server
 const server = http.createServer((request, response) => {
-  // response.setHeader('Content-Type', 'application/json');
-  // response.writeHead(201, { 'Content-Type': 'text/plain' });
-  // response.writeHead(200, { 'Content-Type': 'application/json' });
-  response.writeHead(200, { 'Content-Type': 'text/html' });
-  response.write(`
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <title>NODEJS</title>
-      </head>
+  const parsedURL = new URL(request.url, `http://${request.headers.host}`);
+  const pathname = parsedURL.pathname;
 
-      <body>
-        <div>
-          <h1>Hello World!</h1>
-          <p>Welcome</p>
-        </div>
-      </body>
-    </hml>
-    `);
-  response.end();
+  if (pathname === '/') {
+    switch (request.method) {
+      case 'GET':
+        response.writeHead(200, { "content-type": 'application/json' });
+        response.write(JSON.stringify({ message: 'hello GET!' }));
+        response.end();
+        break;
+      case 'POST':
+        response.writeHead(200, { "content-type": 'application/json' });
+        response.write(JSON.stringify({ message: 'hello POST!' }));
+        response.end();
+        break;
+      case 'PUT':
+        response.writeHead(200, { "content-type": 'application/json' });
+        response.write(JSON.stringify({ message: 'hello PUT!' }));
+        response.end();
+        break;
+      case 'DELETE':
+        response.writeHead(200, { "content-type": 'application/json' });
+        response.write(JSON.stringify({ message: 'hello DELETE!' }));
+        response.end();
+        break;
+      default:
+        response.writeHead(404, { "content-type": 'application/json' });
+        response.write(JSON.stringify({ message: 'unknown http method' }));
+        response.end();
+        break;
+    }    
+  } else if (pathname === '/users') {
+    response.writeHead(200, { "content-type": 'application/json' });
+    response.write(JSON.stringify({ message: 'hello users!' }));
+    response.end();
+  } else {
+    response.writeHead(404, { "content-type": 'application/json' });
+    response.write(JSON.stringify({ message: 'unknown route' }));
+    response.end();
+  }
 });
 
 // run the http server
